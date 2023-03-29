4dee3d19ed34241588a08b7bbfe3bb20ae45314a
Felipe Santos <106451519+Santos013@users.noreply.github.com>
2023-03-29 01:47:48 -0300

    Add files via upload

---
+++ ChapterFS11/ChapterFS11/ViewModel/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChapterFS11.ViewModel
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Informe e-mail do usuário")]
+        public string email { get; set; }
+
+        [Required(ErrorMessage = "Informe senha do usuário")]
+        public string senha { get; set; }
+    }
+}
